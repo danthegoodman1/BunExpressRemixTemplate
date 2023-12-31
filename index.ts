0cf443f939f56e579ccd080b247934c68b2ac2ac
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-12-31 08:45:07 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,92 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
+import cors from 'cors'
+
+import { logger } from './logger/index'
+import { ConnectDB } from './db/index'
+import { createRequestHandler } from "@remix-run/express"
+
+import * as build from "./build/index.js"
+
+const listenPort = process.env.PORT || '8080'
+
+declare global {
+  namespace Express {
+    interface Request {
+      id: string
+    }
+  }
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      API_TOKEN: string
+      DSN: string
+    }
+  }
+}
+
+async function main() {
+
+  const app = express()
+  app.use(express.json())
+  app.disable('x-powered-by')
+  app.use(cors())
+
+  ConnectDB()
+
+  // Remix public
+  app.use(express.static("public"))
+
+  app.use((req, res, next) => {
+    const reqID = uuidv4()
+    req.id = reqID
+    next()
+  })
+
+  if (process.env.HTTP_LOG === "1") {
+    logger.debug("using HTTP logger")
+    app.use((req: any, res, next) => {
+      req.log.info({ req })
+      res.on("finish", () => req.log.info({ res }))
+      next()
+    })
+  }
+
+  app.get('/hc', (req, res) => {
+    res.sendStatus(200)
+  })
+
+  // Everything else we send to the frontend
+  app.all("*", createRequestHandler({ build: build as any }))
+
+  const server = app.listen(listenPort, () => {
+    logger.info(`API listening on port ${listenPort}`)
+  })
+
+  let stopping = false
+
+  process.on('SIGTERM', async () => {
+    if (!stopping) {
+      stopping = true
+      logger.warn('Received SIGTERM command, shutting down...')
+      server.close()
+      logger.info('exiting...')
+      process.exit(0)
+    }
+  })
+
+  process.on('SIGINT', async () => {
+    if (!stopping) {
+      stopping = true
+      logger.warn('Received SIGINT command, shutting down...')
+      server.close()
+      logger.info('exiting...')
+      process.exit(0)
+    }
+  })
+}
+
+main()
