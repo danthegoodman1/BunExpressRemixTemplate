2e444619efaa88e469c3a95823648595d1d0e57f
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-01-13 12:29:43 -0500

    logger update

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,19 +1,22 @@
-import {pino} from "pino"
+import { pino } from "pino"
 
-export const logger = pino(process.env.DEBUG === "1" ? {
-  transport: {
-    target: 'pino-pretty',
-  },
-  level: process.env.LOG_LEVEL || 'debug',
-
-} : {
-  level: process.env.LOG_LEVEL || 'info',
-  formatters: {
-    level: (label) => {
-      return {
-        [process.env.LOG_LEVEL_KEY || 'level']: label
+export const logger = pino(
+  process.env.PRETTY === "1"
+    ? {
+        transport: {
+          target: "pino-pretty",
+        },
+        level: process.env.LOG_LEVEL || "debug",
       }
-    },
-  },
-  messageKey: process.env.LOG_MSG_KEY || "message"
-})
+    : {
+        level: process.env.LOG_LEVEL || "info",
+        formatters: {
+          level: (label) => {
+            return {
+              [process.env.LOG_LEVEL_KEY || "level"]: label,
+            }
+          },
+        },
+        messageKey: process.env.LOG_MSG_KEY || "message",
+      }
+)
